650219653aa26373336680add85a72d8c3f1d881
plnyakan <plnyakan@student.wethinkcode.co.za>
2022-04-28 11:41:54 +0200

    added authentication component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
     signInWithPopup,
     GoogleAuthProvider,
     createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
 } from 'firebase/auth'
 
 import {
@@ -75,5 +76,12 @@ const firebaseConfig = {
     if(!email || !password) return;
 
     return await createUserWithEmailAndPassword(auth,email,password);
+
+  };
+
+  export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth,email,password);
 
   };
